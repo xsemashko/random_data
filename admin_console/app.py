743a7a3c7b4f759c5d4739c2cb664b77309bffe2
xsemashko <ollien_sk8@mail.ru>
2025-02-28 14:18:43 +0300

    Update app.py

--- admin_console/app.py
+++ admin_console/app.py
@@ -1,3 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+import psycopg2
+
+app = Flask(__name__)
+
+# Настройки подключения к базе данных
+DB_CONFIG = {
+    'dbname': 'your_dbname',
+    'user': 'your_username',
+    'password': 'your_password',
+    'host': 'localhost',
+    'port': 5432
+}
+
+def get_db_connection():
+    conn = psycopg2.connect(**DB_CONFIG)
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/get_data')
 def get_data():
     conn = get_db_connection()
@@ -41,3 +63,35 @@ def get_data():
         'users': users,
         'apps': apps
     })
+
+@app.route('/update_data', methods=['POST'])
+def update_data():
+    data = request.json
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    try:
+        if data['table'] == 'reg_requests':
+            cur.execute('''
+                UPDATE webplatform.reg_requests
+                SET status = %s
+                WHERE id = %s
+            ''', (data.get('status'), data['id']))
+        elif data['table'] == 'users':
+            cur.execute('''
+                UPDATE webplatform.users
+                SET mail = %s, status = %s, grants = %s
+                WHERE id = %s
+            ''', (data.get('mail'), data.get('status'), data.get('grants'), data['id']))
+
+        conn.commit()
+        return jsonify({'success': True})
+    except Exception as e:
+        conn.rollback()
+        return jsonify({'success': False, 'error': str(e)})
+    finally:
+        cur.close()
+        conn.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
